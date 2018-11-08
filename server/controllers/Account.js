4267d639c2bf9b72fca823be3a616412a720361f
Steve Callen <src3363@rit.edu>
2018-11-08 13:35:34 -0500

    this stuff almost works

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -3,18 +3,19 @@ const models = require('../models');
 const Account = models.Account;
 
 const loginPage = (req, res) => {
-  res.render('login');
+  res.render('login', { csrfToken: req.csrfToken() });
 };
 
 const signupPage = (req, res) => {
-  res.render('signup');
+  res.render('signup', { csrfToken: req.csrfToken() });
 };
 
 const changePWPage = (req, res) => {
-  res.render('pwchange');
+  res.render('pwchange', { csrfToken: req.csrfToken() });
 };
 
 const logout = (req, res) => {
+  req.session.destroy();
   res.redirect('/');
 };
 
@@ -33,6 +34,8 @@ const login = (request, response) => {
     if(err || !account) {
       return res.status(401).json({ error: 'Wrong username or password' });
     }
+    
+    req.session.account = Account.AccountModel.toAPI(account);
     
     return res.json({ redirect: '/game' });
   });
@@ -65,7 +68,10 @@ const signup = (request, response) => {
     
     const savePromise = newAccount.save();
     
-    savePromise.then(() => res.json({redirect: '/game'}));
+    savePromise.then(() => {
+      req.session.account = Account.AccountModel.toAPI(newAccount);
+      return res.json({redirect: '/game'});
+    });
     
     savePromise.catch((err) => {
       console.log(err);
--- server/router.js
+++ server/router.js
@@ -1,15 +1,18 @@
 const controllers = require('./controllers');
+const mid = require('./middleware');
 
 const router = (app) => {
-  app.get('/login', controllers.Account.loginPage);
-  app.post('/login', controllers.Account.login);
-  app.get('/signup', controllers.Account.signupPage);
-  app.post('/signup', controllers.Account.signup);
-  app.get('/logout', controllers.Account.logout);
-  app.get('/changePassword', controllers.Account.changePWPage);
-  app.post('/changePassword', controllers.Account.changePW);
-  app.get('/game', controllers.ElderGod.gamePage);
-  app.get('/', controllers.Account.loginPage);
+  app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
+  app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
+  app.get('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signupPage);
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+  app.get('/logout', mid.requiresLogin, controllers.Account.logout);
+  app.get('/changePassword', mid.requiresSecure, mid.requiresLogin, controllers.Account.changePWPage);
+  app.post('/changePassword', mid.requiresSecure, mid.requiresLogin, controllers.Account.changePW);
+  app.get('/game', mid.requiresLogin, controllers.ElderGod.gamePage);
+  app.post('/game', mid.requiresLogin, controllers.ElderGod.newGame);
+  //app.post('/gameSave', mid.requiresLogin, controllers.ElderGod.save);
+  app.get('/', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
 };
 
 module.exports = router;
